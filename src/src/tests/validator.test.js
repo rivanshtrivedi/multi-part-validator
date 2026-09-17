ab0932165eacf5968b9c4bf68baaa198a1996537
rivanshtrivedi <257249234+rivanshtrivedi@users.noreply.github.com>
2026-09-17 20:57:06 +0530

    Delete src directory

--- src/src/tests/validator.test.js
+++
@@ -1,5 +0,0 @@
-const { validateStructure } = require('../src/validator');
-test('Validates correctly', () => {
-  const schema = { id: { required: true } };
-  expect(validateStructure(schema, { id: 1 }).isValid).toBe(true);
-});
